04e6c1ceeea15332b4e75de01c26b2d013e78140
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-07-09 21:59:28 -0500

    super puta

--- app/Erp/Services/TransaccionService.php
+++ app/Erp/Services/TransaccionService.php
@@ -29,13 +29,49 @@ class TransaccionService
     public function getRubroRendimiento()
     {
         // return "eso lisin";
-        $users = DB::table('rubros')
+        $results = DB::table('rubros')
             ->join('transaccions', 'transaccions.rubro_id', '=', 'rubros.rubro_id')
-            ->select('rubros.rubro_id','rubros.nombre', 'transaccions.descripcion', DB::raw('COUNT(transaccions.transaccion_id) as total_transacciones'))
-            ->groupBy('rubros.rubro_id','rubros.nombre','transaccions.descripcion')
+            ->select('rubros.rubro_id', 'rubros.nombre', 'transaccions.descripcion', 'transaccions.transaccion_id')
+            ->groupBy('rubros.rubro_id', 'rubros.nombre', 'transaccions.descripcion', 'transaccions.transaccion_id')
             ->get();
 
-        return $users;
+        // return $results;
+        // $transacciones = Transaccion::where('rubro_id')->get();
+
+        $trans = DB::table('transaccions')
+            ->join('rubros', 'rubros.rubro_id', '=', 'transaccions.rubro_id')
+            ->get();
+        $myTransaccion = [];
+        // dd($trans);
+        foreach ($trans as $userId => $userRow) {
+
+            // El conteo se saca contando los elementos agrupados en la colección
+            // $totalPosts = $userRow->count();
+            // $totalPosts = collect($userRow); 
+            // $userName = $userRow->first()->user_name;
+            //  dd($userCollection);
+            $userCollection = collect($userRow);
+            // $userName = "la transaccion: ";
+            // dd($userRow);
+
+            foreach ($userRow as $row) {
+                //  dd($userRow, "la row:", $row);
+                // echo "- Post: " . $row->descripcion . "<br>";
+                $myTransaccion = $row;
+            }
+
+
+            // foreach ($userRow as $row) {
+            //     echo "- Post: " . $row->title . "<br>";
+            // }
+        }
+
+        // dd("lass tranacciones", $userRow);
+        return [
+            $results,
+            "cada transaccion" => $userRow
+        ];
+        // return $users;
 
     }
 }
